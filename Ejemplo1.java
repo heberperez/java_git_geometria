8a76d533ab2f845881960f3d444b3f1f41057238
heber perez <hpgl19960502_moises@hotmail.com>
2018-08-01 12:12:21 -0500

    Se crea el main junto cn tres circulos 2D

---
+++ Ejemplo1.java
@@ -0,0 +1,16 @@
+
+import java.util.Vector;
+import java.awt.*;
+
+public class Ejemplo1{
+	public static void main(String arg[] throws InterruptedException){
+		System.out.println("comienza main()...");
+		Circulo c =new Circulo(2.0,2.0,4.0);
+		System.out.println("Radio = " + c.r + " unidades.");
+		System.out.println("Centro = ("+c.x+","+c.y+")unidades.");
+		Circulo c1=new Circulo();
+		Circulo c2=new Circulo();	
+
+
+	}
+}
